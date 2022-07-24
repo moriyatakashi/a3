fb5cb742252b533a8ad1f94ee2bf997d6d79a340
moriyatakashi <36041907+moriyatakashi@users.noreply.github.com>
2022-07-24 19:44:24 +0900

    07241930

--- a1.sh
+++ a1.sh
@@ -1,1 +1,13 @@
 apt update
+apt -y install wget sudo gnupg2 software-properties-common git curl
+wget -qO- https://adoptopenjdk.jfrog.io/adoptopenjdk/api/gpg/key/public|sudo apt-key add -
+sudo add-apt-repository --yes https://adoptopenjdk.jfrog.io/adoptopenjdk/deb
+sudo apt-get update
+sudo apt-get -y install adoptopenjdk-8-hotspot
+apt -y install maven
+git clone https://github.com/moriyatakashi/nablarch-example-web.git
+mv nablarch-example-web root
+cd root/nablarch-example-web
+mvn generate-resources
+mvn compile
+mvn waitt:run
